a62f18512425b19966e749a80a2c6c0fef65fc99
bonnibi <beini_pan@brown.edu>
2014-06-22 23:58:29 -0700

    made delete button and other things

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var core = freedom.core();
 var social = freedom.socialprovider();
 var storage = freedom.storageprovider();
 
-var files = {};  
+var files = {};
 
 freedom.on('add_paper', function(data) {
   console.log(data.key.toString());
@@ -16,7 +16,7 @@ freedom.on('add_paper', function(data) {
   };
 
   freedom.emit('added_paper', data);
-  /*var promise = store.get('myPapers');
+  var promise = storage.get('myPapers');
   promise.then(function(val) {
     var currPapers, prevCurrPapers;
     var newPaper = {}; 
@@ -38,12 +38,34 @@ freedom.on('add_paper', function(data) {
       console.log(currPapers[i].date + " AND " + currPapers[i].title); 
 
     if(currPapers !== prevCurrPapers)
-      store.set('myPapers', JSON.stringify(currPapers)); 
+      storage.set('myPapers', JSON.stringify(currPapers)); 
 
     freedom.emit('added_paper', currPapers); 
 
-  }); */ 
+  }); 
 });
+freedom.on('delete_paper', function(paper) {
+  console.log("deleting paper" + paper.title);
 
+  var promise = storage.get('myPapers');
+  promise.then(function(val) {
+    var currPapers, prevCurrPapers;
+    var newPaper = {};
 
+    try {
+      currPapers = JSON.parse(val);
+      prevCurrPapers = JSON.parse(val); 
+    } catch(e) {}  
 
+    if (!currPapers || typeof currPapers !== "object") 
+      currPapers = []; 
+
+    for(var i = 0; i < currPapers.length; i++){
+      if(currPapers[i].title === paper.title) {
+        currPapers.splice(i, 1);
+        break;
+      }
+    }
+    storage.set('myPapers', JSON.stringify(currPapers));
+  });
+});
--- ux.js
+++ ux.js
@@ -9,12 +9,22 @@ app.controller('main_controller', function($scope, $http, $modal, $window) {
   $scope.paperOptions = { 
     data: 'myPapers',
     multiSelect: false,
-    columnDefs: [{field: 'date', displayName: 'date'}, 
-    {field: 'title', displayName: 'title'},
-    {displayName: '', cellTemplate: '<button id="viewPaperBtn" type="button" ng-click="viewPaper(row.entity)">view paper</button>'}]
+    columnDefs: [
+    { field: 'title', displayName: 'title' },
+    { field: 'date', displayName: 'date' }, 
+    { displayName: '', cellTemplate: '<button id="viewPaperBtn" type="button" ng-click="viewPaper(row.entity)">view paper</button>'},
+    { displayName: '', cellTemplate: '<button id="deletePaperBtn" type="button" ng-click="deletePaper(row.entity, $index)">delete paper</button>'}]
   };
 
   $scope.reviewOptions = { data: 'myReviews' };
+
+  $scope.deletePaper = function(paper, index) {
+    window.freedom.emit('delete_paper', {
+      title : paper.title
+    });
+    var index = this.row.rowIndex;
+    $scope.myPapers.splice(index, 1);
+  }
 
   $scope.viewPaper = function(paper) {
     var modalInstance = $modal.open({
@@ -41,13 +51,14 @@ app.controller('main_controller', function($scope, $http, $modal, $window) {
   $scope.addPaper = function() {
     var modalInstance = $modal.open({
       templateUrl: 'addPaperTemplate.html',
+      windowClass:'normal',
       controller: addPaperCtrl,
       size: 'lg',
       backdrop: 'static'
     });
   };
 
-  $window.freedom.on('added_paper', function(data) {
+  $window.freedom.on('added_paper', function(data) {//necessary?
     var dd = data.date.getDate();
     var mm = data.date.getMonth()+1; 
     var yyyy = data.date.getFullYear();
@@ -55,6 +66,9 @@ app.controller('main_controller', function($scope, $http, $modal, $window) {
         date: yyyy+'-'+mm+'-'+dd,
         title: data.title
     });
+
+    var file = data.value;
+
   }); 
 });
 
@@ -69,43 +83,40 @@ var viewPaperCtrl = function($scope, $modalInstance, title) {
 var addPaperCtrl = function ($scope, $modalInstance) {
   $scope.upload = function () {
     var fileArray = document.getElementById('addFile').files; 
-    uploadFile(fileArray);  
-    $modalInstance.close(fileArray);
+    if (!fileArray.length) {
+      alert('Must select a file.');
+      return;
+    }
+
+    var today = new Date();  
+    var key = Math.random() + "";
+    var newPaper = fileArray[0]; 
+    var fileReader = new FileReader(); 
+
+    console.log("a file found: " + newPaper.name);
+
+//    var fileBlob = newPaper.slice(0, newPaper.size);
+//    fileReader.readAsBinaryString(fileBlob);
+
+//    fileReader.onloadend = function(evt) {
+//      if (evt.target.readyState == FileReader.DONE) { // DONE == 2
+
+        window.freedom.emit('add_paper', {
+          title: newPaper.name,
+          value: newPaper, 
+          date: today,
+//          content: evt.target.result,
+          key: key
+        });
+//      }
+//    };
+  $modalInstance.close();
   };
 
   $scope.cancel = function () {
     $modalInstance.dismiss('cancel');
   };
-}; 
-
-function uploadFile(fileArray) { 
-  if(fileArray.length < 1) {
-    console.error("no file found");
-    document.getElementById("fileError").textContent = "no paper uploaded..."; 
-    return; 
-  }
-  document.getElementById('fileError').textContent = "";
-
-  var newPaper = fileArray[0]; 
-  var fileReader = new FileReader(); 
-
-  console.log("a file found: " + newPaper.name);
-
-  var today = new Date();  
-  var key = Math.random() + "";
-
-  window.freedom.emit('add_paper', {
-    title: newPaper.name,
-    value: newPaper, 
-    date: today,
-    key: key 
-  });
-} 
+};
 
 window.onload = function() {
-
 }; 
-
-
-
-  
