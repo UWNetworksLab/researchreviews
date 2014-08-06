790945f5407abeacaa57d29ce3d641e04d14413c
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2014-08-06 14:24:37 -0700

    linked alerts

--- javascript/uxalerts.js
+++ javascript/uxalerts.js
@@ -1,32 +1,40 @@
-app.controller('alertsController', function($scope, $controller) {
+app.controller('alertsController', function($scope, $controller, $location) {
   $scope.showNav = true; 
 	$scope.alerts;
   $scope.alertNum = alertNum; 
 
 	$scope.init = function(){
 		messageList.forEach(function(msg){
+      var alert = {}; 
       if (msg.action === 'invite-reviewer') {
         if (!$scope.alerts) $scope.alerts = [];
-        var alertMsg = 'You have been invited to review the paper ' + msg.title + ' v.' + msg.vnum + ' by ' + msg.author;
-        if(oldMessageList.indexOf(alertMsg) == -1)
-          oldMessageList.unshift(alertMsg);
+        alert.msg = 'You have been invited to review the paper ' + msg.title + ' v.' + msg.vnum + ' by ' + msg.author;
+        alert.action = "reviewspage"; 
+        if(oldMessageList.indexOf(alert) == -1)
+          oldMessageList.unshift(alert);
       }
       else if (msg.action === 'add-review-on-author') {
         if (!$scope.alerts) $scope.alerts = [];
         msg.date = new Date(msg.date); 
-        var alertMsg = msg.reviewer + ' reviewed your paper ' + msg.title + ' v.' + msg.vnum + ' on ' + msg.date.getFullYear() + "-" + (msg.date.getMonth()+1) + "-" + msg.date.getDate();
-        if(oldMessageList.indexOf(alertMsg) == -1)
-          oldMessageList.unshift(alertMsg);
+        alert.msg = msg.reviewer + ' reviewed your paper ' + msg.title + ' v.' + msg.vnum + ' on ' + msg.date.getFullYear() + "-" + (msg.date.getMonth()+1) + "-" + msg.date.getDate();
+        alert.action = "paperspage";
+        if(oldMessageList.indexOf(alert) == -1)
+          oldMessageList.unshift(alert);
       }
       else if(msg.action === 'invite-group') {
         if($scope.alerts) $scope.alerts = []; 
-        var alertMsg = "You have been invited by " + msg.from + " to join the group " + msg.groupName; 
-        if(oldMessageList.indexOf(alertMsg) == -1)
-          oldMessageList.unshift(alertMsg); 
+        alert.msg = "You have been invited by " + msg.from + " to join the group " + msg.groupName; 
+        alert.action = "groupspage"; 
+        if(oldMessageList.indexOf(alert) == -1)
+          oldMessageList.unshift(alert); 
       }
 		});
     $scope.alerts = oldMessageList;
 		messageList = [];
 	};
 	$scope.init(); 
+
+  $scope.changeView = function(view){
+    $location.path(view);  
+  };
 });
